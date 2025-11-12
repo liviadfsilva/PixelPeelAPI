2166e4523cdc6b0f3495a8db647dbb422ac5770c
liviadfsilva <liviadfsilva@gmail.com>
2025-11-12 02:34:54 -0300

    first commit

---
+++ src/main/java/com/liviadfsilva/pixelpeel/PixelpeelApplication.java
@@ -0,0 +1,13 @@
+package com.liviadfsilva.pixelpeel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelpeelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelpeelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liviadfsilva/pixelpeel/PixelpeelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liviadfsilva.pixelpeel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelpeelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
