80f3da7849f3dccd55bfd095969ea55ff78d969d
liviadfsilva <liviadfsilva@gmail.com>
2025-11-24 15:45:46 -0300

    feat: enable JpaAuditing

--- src/main/java/com/liviadfsilva/pixelpeel/config/SecurityConfig.java
+++ src/main/java/com/liviadfsilva/pixelpeel/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.liviadfsilva.pixelpeel.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableJpaAuditing
 public class SecurityConfig {
     
     @Bean
