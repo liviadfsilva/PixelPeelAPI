e55b077ae0214219e6866ccbe9929bce1c03b878
liviadfsilva <liviadfsilva@gmail.com>
2025-11-26 14:04:59 -0300

    feat: add createdAt and updatedAt

--- src/main/java/com/liviadfsilva/pixelpeel/User/model/User.java
+++ src/main/java/com/liviadfsilva/pixelpeel/User/model/User.java
@@ -1,12 +1,23 @@
 package com.liviadfsilva.pixelpeel.User.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.liviadfsilva.pixelpeel.Cart.model.Cart;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,6 +29,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "users")
 public class User {
 
@@ -40,4 +52,13 @@ public class User {
 
     @Column(nullable = false)
     private Boolean isActive;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
+    private Cart cart;
 }
