789b6f790cbe5c8243defd04b8c138ed12d7fa36
liviadfsilva <liviadfsilva@gmail.com>
2025-11-26 15:13:26 -0300

    feat: create relationship between User and Order

--- src/main/java/com/liviadfsilva/pixelpeel/User/model/User.java
+++ src/main/java/com/liviadfsilva/pixelpeel/User/model/User.java
@@ -1,13 +1,17 @@
 package com.liviadfsilva.pixelpeel.User.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.liviadfsilva.pixelpeel.Cart.model.Cart;
+import com.liviadfsilva.pixelpeel.Order.model.Order;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -17,6 +21,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -61,4 +66,7 @@ public class User {
 
     @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
     private Cart cart;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders = new ArrayList<>();
 }
