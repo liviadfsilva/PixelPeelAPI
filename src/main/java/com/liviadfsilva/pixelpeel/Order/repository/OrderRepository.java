3d52e70727e84118b45d5660bd46a180dbab7bcd
liviadfsilva <liviadfsilva@gmail.com>
2025-12-02 14:52:53 -0300

    feat: add findByUserId

--- src/main/java/com/liviadfsilva/pixelpeel/Order/repository/OrderRepository.java
+++ src/main/java/com/liviadfsilva/pixelpeel/Order/repository/OrderRepository.java
@@ -1,12 +1,13 @@
-package com.liviadfsilva.pixelpeel.Cart.repository;
+package com.liviadfsilva.pixelpeel.Order.repository;
 
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.liviadfsilva.pixelpeel.Cart.model.Cart;
+import com.liviadfsilva.pixelpeel.Order.model.Order;
 
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    
     Optional<Cart> findByUserId(Long userId);
 }
