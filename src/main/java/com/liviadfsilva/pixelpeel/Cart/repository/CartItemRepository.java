b60731c937e72f73400fafc1edc64c828fd870cc
liviadfsilva <liviadfsilva@gmail.com>
2026-01-05 15:46:42 -0300

    fix: remove Cart import

--- src/main/java/com/liviadfsilva/pixelpeel/Cart/repository/CartItemRepository.java
+++ src/main/java/com/liviadfsilva/pixelpeel/Cart/repository/CartItemRepository.java
@@ -3,13 +3,11 @@ package com.liviadfsilva.pixelpeel.Cart.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.liviadfsilva.pixelpeel.Cart.model.Cart;
 import com.liviadfsilva.pixelpeel.Cart.model.CartItem;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     
-    Optional<Cart> findByUserId(Long userId);
+    Optional<CartItem> findByCartUserId(Long userId);
 
     Optional<CartItem> findByCartIdAndStickerId(Long cartId, Long stickerId);
 
