edc777608ccb165a1e816d92ba1aab03836560ce
liviadfsilva <liviadfsilva@gmail.com>
2026-01-05 17:35:04 -0300

    fix: replace io.swagger RequestBody import for org.spring RequestBody

--- src/main/java/com/liviadfsilva/pixelpeel/Auth/controller/AuthController.java
+++ src/main/java/com/liviadfsilva/pixelpeel/Auth/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.liviadfsilva.pixelpeel.Auth.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.liviadfsilva.pixelpeel.Auth.jwt.TokenBlacklistService;
 import com.liviadfsilva.pixelpeel.Auth.model.LoginRequest;
 import com.liviadfsilva.pixelpeel.Auth.service.AuthService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/auth")
