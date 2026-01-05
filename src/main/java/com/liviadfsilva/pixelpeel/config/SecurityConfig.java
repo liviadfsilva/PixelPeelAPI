1fd4def67729cebab971990281b2c17faa5b9623
liviadfsilva <liviadfsilva@gmail.com>
2026-01-05 17:29:43 -0300

    feat: allow access to users/create-admin route

--- src/main/java/com/liviadfsilva/pixelpeel/config/SecurityConfig.java
+++ src/main/java/com/liviadfsilva/pixelpeel/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**", "/error", "/swagger-ui/**", "/v3/api-docs/**", "/users/register").permitAll()
+                .requestMatchers("/auth/**", "/error", "/swagger-ui/**", "/v3/api-docs/**", "/users/register", "/users/create-admin").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
