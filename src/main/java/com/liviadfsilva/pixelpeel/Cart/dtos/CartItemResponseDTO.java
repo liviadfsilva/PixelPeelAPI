bf1484288c24fd16d74badb1091a37447b91e457
liviadfsilva <liviadfsilva@gmail.com>
2026-01-06 15:47:42 -0300

    feat: create responde DTOs

---
+++ src/main/java/com/liviadfsilva/pixelpeel/Cart/dtos/CartItemResponseDTO.java
@@ -0,0 +1,24 @@
+package com.liviadfsilva.pixelpeel.Cart.dtos;
+
+import java.math.BigDecimal;
+
+import com.liviadfsilva.pixelpeel.Cart.model.CartItem;
+
+import lombok.Getter;
+
+@Getter
+public class CartItemResponseDTO {
+    
+    private final Long stickerId;
+    private final BigDecimal price;
+    private final int quantity;
+    private final BigDecimal lineTotal;
+
+    public CartItemResponseDTO(CartItem item) {
+        this.stickerId = item.getSticker().getId();
+        this.price = item.getPrice();
+        this.quantity = item.getQuantity();
+        this.lineTotal = price.multiply(BigDecimal.valueOf(quantity));
+
+    }
+}
---
+++ src/main/java/com/liviadfsilva/pixelpeel/Cart/dtos/CartResponseDTO.java
@@ -0,0 +1,28 @@
+package com.liviadfsilva.pixelpeel.Cart.dtos;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.liviadfsilva.pixelpeel.Cart.model.Cart;
+
+import lombok.Getter;
+
+@Getter
+public class CartResponseDTO {
+    private final Long id;
+    private final Long userId;
+    private final List<CartItemResponseDTO> items;
+    private final BigDecimal totalPrice;
+
+    public CartResponseDTO(Cart cart) {
+        this.id = cart.getId();
+        this.userId = cart.getUser().getId();
+        this.items = cart.getItems()
+                .stream()
+                .map(CartItemResponseDTO::new)
+                .toList();
+        this.totalPrice = items.stream()
+                .map(CartItemResponseDTO::getLineTotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+}
