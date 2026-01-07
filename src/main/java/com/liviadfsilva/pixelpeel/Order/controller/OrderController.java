da6db3625d162aeb3ab4ccaed73197d3adaaa09e
liviadfsilva <liviadfsilva@gmail.com>
2026-01-07 16:21:59 -0300

    feat: add @PatchMapping and replace Optional<Order> for ResponseEntity<Order>

--- src/main/java/com/liviadfsilva/pixelpeel/Order/controller/OrderController.java
+++ src/main/java/com/liviadfsilva/pixelpeel/Order/controller/OrderController.java
@@ -1,5 +1,8 @@
 package com.liviadfsilva.pixelpeel.Order.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.liviadfsilva.pixelpeel.Order.model.Order;
+import com.liviadfsilva.pixelpeel.Order.model.OrderStatus;
 import com.liviadfsilva.pixelpeel.Order.model.PaymentStatus;
 import com.liviadfsilva.pixelpeel.Order.service.OrderService;
 
@@ -20,17 +24,36 @@ public class OrderController {
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Order> getAllOrdersByUserId(@PathVariable Long userId){
+        return orderService.getAllOrdersByUserId(userId)
+            .map(ResponseEntity::ok)
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity<Order> getOrderById(@PathVariable Long orderId) {
+        return orderService.getOrderById(orderId)
+            .map(ResponseEntity::ok)
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
     
     @PostMapping("/create/{userId}")
     public Order createOrder(@PathVariable Long userId) {
         return orderService.createOrder(userId);
     }
 
-    @PutMapping("/{orderId}/status")
+    @PutMapping("/{orderId}/payment-status/order-status")
     public Order updateStatus(
             @PathVariable Long orderId,
-            @RequestParam PaymentStatus status
+            @RequestParam PaymentStatus paymentStatus,
+            @RequestParam OrderStatus orderStatus
     ) {
-        return orderService.updatePaymentStatus(orderId, status);
+        return orderService.updateStatus(orderId, paymentStatus, orderStatus);
+    }
+
+    @PatchMapping("/cancel/{orderId}")
+    public Order cancelOrder(@PathVariable Long orderId){
+        return orderService.cancelOrder(orderId);
     }
 }
