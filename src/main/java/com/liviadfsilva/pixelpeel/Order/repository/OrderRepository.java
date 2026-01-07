21ba3bd9fe5f257332a147b4484bb6ea27b6bdf0
liviadfsilva <liviadfsilva@gmail.com>
2026-01-07 15:58:37 -0300

    fix: replace List for Optional

--- src/main/java/com/liviadfsilva/pixelpeel/Order/repository/OrderRepository.java
+++ src/main/java/com/liviadfsilva/pixelpeel/Order/repository/OrderRepository.java
@@ -1,11 +1,11 @@
 package com.liviadfsilva.pixelpeel.Order.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.liviadfsilva.pixelpeel.Order.model.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    List<Order> findAllByUserId(Long userId);
+    Optional<Order> findAllByUserId(Long userId);
 }
