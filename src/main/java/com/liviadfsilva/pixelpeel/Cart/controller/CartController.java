0912f423d7870a541afa606fe73e085e9521242c
liviadfsilva <liviadfsilva@gmail.com>
2026-01-08 14:55:06 -0300

    fix: replace @PutMapping for @PatchMapping

--- src/main/java/com/liviadfsilva/pixelpeel/Cart/controller/CartController.java
+++ src/main/java/com/liviadfsilva/pixelpeel/Cart/controller/CartController.java
@@ -2,9 +2,9 @@ package com.liviadfsilva.pixelpeel.Cart.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +39,7 @@ public class CartController {
         return new CartResponseDTO(cart);
     }
 
-    @PutMapping("/{userId}/update")
+    @PatchMapping("/{userId}/update")
     public CartResponseDTO updateItem(
             @PathVariable Long userId,
             @RequestParam Long stickerId,
--- src/main/java/com/liviadfsilva/pixelpeel/Sticker/controller/StickerController.java
+++ src/main/java/com/liviadfsilva/pixelpeel/Sticker/controller/StickerController.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,7 +46,7 @@ public class StickerController {
         return service.createSticker(sticker);
     }
 
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     public Sticker updateSticker(@PathVariable Long id, @RequestBody Sticker sticker) {
         return service.updateSticker(id, sticker);
     }
