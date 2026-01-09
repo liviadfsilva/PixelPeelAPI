03140fc6da30d15a727ad04bc0ac5a82223531a1
liviadfsilva <liviadfsilva@gmail.com>
2026-01-09 14:36:07 -0300

    fix: remove Optional import

--- src/main/java/com/liviadfsilva/pixelpeel/Order/repository/OrderRepository.java
+++ src/main/java/com/liviadfsilva/pixelpeel/Order/repository/OrderRepository.java
@@ -1,11 +1,11 @@
 package com.liviadfsilva.pixelpeel.Order.repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.liviadfsilva.pixelpeel.Order.model.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    Optional<Order> findAllByUserId(Long userId);
+    List<Order> findAllByUserId(Long userId);
 }
