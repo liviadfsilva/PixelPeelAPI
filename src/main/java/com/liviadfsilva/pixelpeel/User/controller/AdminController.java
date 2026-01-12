674cff304a078486d281efda40a8cd807aa124fd
liviadfsilva <liviadfsilva@gmail.com>
2026-01-12 16:33:32 -0300

    feat: create AdminController

---
+++ src/main/java/com/liviadfsilva/pixelpeel/User/controller/AdminController.java
@@ -0,0 +1,86 @@
+package com.liviadfsilva.pixelpeel.User.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.liviadfsilva.pixelpeel.User.dto.UserRegistrationDTO;
+import com.liviadfsilva.pixelpeel.User.dto.UserResponseDTO;
+import com.liviadfsilva.pixelpeel.User.dto.UserUpdateDTO;
+import com.liviadfsilva.pixelpeel.User.model.Role;
+import com.liviadfsilva.pixelpeel.User.model.User;
+import com.liviadfsilva.pixelpeel.User.service.UserService;
+
+@RestController
+@RequestMapping("/admin/users")
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminController {
+
+    private final UserService service;
+
+    public AdminController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+
+        List<UserResponseDTO> users = service.getAllUsers()
+            .stream()
+            .map(user -> new UserResponseDTO(user.getName(), user.getEmail()))
+            .toList();
+
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<UserResponseDTO> getUserByEmail(@PathVariable String email) {
+
+        return service.getUserByEmail(email)
+            .map(user -> new UserResponseDTO(user.getName(), user.getEmail()))
+            .map(ResponseEntity::ok)
+            .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/role/{role}")
+    public ResponseEntity<List<UserResponseDTO>> getUserByRole(@PathVariable Role role) {
+
+        List<UserResponseDTO> users = service.getUserByRole(role)
+            .stream()
+            .map(user -> new UserResponseDTO(user.getName(), user.getEmail()))
+            .toList();
+
+        return ResponseEntity.ok(users);
+    }
+
+    @PostMapping("/create-admin")
+    public ResponseEntity<UserResponseDTO> createAdmin(@RequestBody UserRegistrationDTO dto) {
+    User user = service.createAdmin(dto.getName(), dto.getEmail(), dto.getPassword());
+
+    UserResponseDTO response = new UserResponseDTO(user.getName(), user.getEmail());
+    return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable Long id, @RequestBody UserUpdateDTO dto) {
+        User user = service.updateUser(id, dto);
+
+        UserResponseDTO response = new UserResponseDTO(user.getName(), user.getEmail());
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        service.softDeleteUser(id);
+    }
+    
+}
